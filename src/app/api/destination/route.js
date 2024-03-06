094976441da5f0afdd02e8f8d819437cabf1e5da
umutakkiran <umutakkiran@gmail.com>
2024-03-06 15:08:52 +0300

    bugfix cors

--- src/app/api/destination/route.js
+++ src/app/api/destination/route.js
@@ -1,8 +1,36 @@
 import { NextResponse } from "next/server";
 import connectMongodb from "../../../../libs/mongodb";
 import Destination from "../../../../models/destination";
+import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware';
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+    Cors({
+      // Allow only requests from the specified domains
+      origin: ['http://example.com', 'http://localhost:3000'],
+      // Allow only certain HTTP methods
+      methods: ['GET', 'POST'],
+    })
+  );
+  
+  // Helper method to wait for a middleware to execute before continuing
+  // And to throw an error when an error happens in a middleware
+  function middleware(handler) {
+    return async (req, res) => {
+      try {
+        await handler(req, res);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Sorry, something went wrong' });
+      }
+    };
+  }
 
 export async function POST (request) {
+    // Run the middleware
+    await middleware(cors);
+
     const data = await request.json(); // Gelen JSON verisini al
     const documents = Array.isArray(data) ? data : [data]; // Gelen veriyi bir diziye dönüştür (eğer tek bir veri gelmişse bile)
     await connectMongodb(); // MongoDB'ye bağlan
@@ -18,6 +46,9 @@ export async function POST (request) {
 }
 
 export async function GET() {
+    // Run the middleware
+     await middleware(cors);
+
     await connectMongodb();
     const destinations = await Destination.find();
     return NextResponse.json({destinations})
@@ -26,6 +57,9 @@ export async function GET() {
 
 
 export async function DELETE(request){
+    // Run the middleware
+    await middleware(cors);
+
     const id = request.nextUrl.searchParams.get("id");
     await connectMongodb();
     await Destination.findByIdAndDelete(id);
