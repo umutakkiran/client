c1f8f415b9eb0cddece23e589594df093b3fac19
umutakkiran <umutakkiran@gmail.com>
2024-03-06 16:01:19 +0300

    bugfix

--- src/app/api/destination/[id]/route.js
+++ src/app/api/destination/[id]/route.js
@@ -1,33 +1,9 @@
 import { NextResponse } from "next/server";
 import connectMongodb from "../../../../../libs/mongodb";
 import Destination from "../../../../../models/destination";
-import Cors from 'cors';
-import initMiddleware from "../../../../../libs/init-middleware";
-// Initialize the cors middleware
-const cors = initMiddleware(
-    Cors({
-      // Allow only requests from the specified domains
-      origin: ['http://example.com', 'http://localhost:3000', "https://seatracker.netlify.app"],
-      // Allow only certain HTTP methods
-      methods: ['GET', 'POST'],
-    })
-  );
-  
-  // Helper method to wait for a middleware to execute before continuing
-  // And to throw an error when an error happens in a middleware
-  function middleware(handler) {
-    return async (req, res) => {
-      try {
-        await handler(req, res);
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Sorry, something went wrong' });
-      }
-    };
-  }
+
 
 export async function GET(request , {params}){
-    await middleware(cors);
 
     const id = params.id.toString();
     console.log(id  )
--- src/app/api/destination/route.js
+++ src/app/api/destination/route.js
@@ -1,34 +1,8 @@
 import { NextResponse } from "next/server";
 import connectMongodb from "../../../../libs/mongodb";
 import Destination from "../../../../models/destination";
-import Cors from 'cors';
-import initMiddleware from "../../../../libs/init-middleware";
-// Initialize the cors middleware
-const cors = initMiddleware(
-    Cors({
-      // Allow only requests from the specified domains
-      origin: ['http://example.com', 'http://localhost:3000' , "https://seatracker.netlify.app"],
-      // Allow only certain HTTP methods
-      methods: ['GET', 'POST'],
-    })
-  );
-  
-  // Helper method to wait for a middleware to execute before continuing
-  // And to throw an error when an error happens in a middleware
-  function middleware(handler) {
-    return async (req, res) => {
-      try {
-        await handler(req, res);
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Sorry, something went wrong' });
-      }
-    };
-  }
 
 export async function POST (request) {
-    // Run the middleware
-    await middleware(cors);
 
     const data = await request.json(); // Gelen JSON verisini al
     const documents = Array.isArray(data) ? data : [data]; // Gelen veriyi bir diziye dönüştür (eğer tek bir veri gelmişse bile)
@@ -45,8 +19,6 @@ export async function POST (request) {
 }
 
 export async function GET() {
-    // Run the middleware
-     await middleware(cors);
 
     await connectMongodb();
     const destinations = await Destination.find();
@@ -56,8 +28,6 @@ export async function GET() {
 
 
 export async function DELETE(request){
-    // Run the middleware
-    await middleware(cors);
 
     const id = request.nextUrl.searchParams.get("id");
     await connectMongodb();
