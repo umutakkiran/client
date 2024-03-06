3909e1fbfb0ae2b4ceba1e344f8bb97d2dc1a553
umutakkiran <umutakkiran@gmail.com>
2024-03-06 14:59:26 +0300

    bugfix

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongodb = async () => {
     try {
-       await mongoose.connect(process.env.MONGO_DB_URI)
+       await mongoose.connect("mongodb+srv://umutakkiran:y0445540@cluster0.b90iwie.mongodb.net/cluster0")
         console.log("Connected")
     } catch (error) {
         console.log(error)
